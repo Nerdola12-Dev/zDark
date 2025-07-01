de4eb85a358c748786e29a508d2997b41b871141
Nerdola12-Dev <nerdolabernardes@gmail.com>
2025-07-01 16:21:06 -0300

    Update zDark_script.py

--- zDark_script.py
+++ zDark_script.py
@@ -10,6 +10,7 @@ MCData.name("zDark_script")
 MCData.clear()
 
 scoreboards.create("godmode", "dummy", Type.start())
+scoreboards.create("ghostmode", "dummy", Type.start())
 #/execute as @a[scores={godmode=1}] at @s run gamemode creative @s
 execute(
     [
@@ -19,14 +20,21 @@ execute(
     "gamemode creative @s",
     Type.loop()
 )
+#GhostMode
+execute(
+    [
+        f"as {Entity.all_players()}[scores={{ghostmode=1}}]",
+        "at @s"
+    ],
+    "gamemode spectator @s",
+    Type.loop()
+)
 #/execute as @a[scores={godmode=0}] at @s run gamemode survival @s
 execute(
     [
-        f"as {Entity.all_players()}[scores={{godmode=0}}]",
+        f"as {Entity.all_players()}[scores={{ghostmode=0, godmode=0}}]",
         "at @s"
     ],
     "gamemode survival @s",
     Type.loop()
 )
-
-MCData.extra_inject()
