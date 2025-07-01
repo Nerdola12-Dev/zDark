99ff270565e7fb80cf88e04e81e2a32bc1881169
Nerdola12-Dev <nerdolabernardes@gmail.com>
2025-06-30 22:45:46 -0300

    zDark.py

--- zDark.py
+++ zDark.py
@@ -4,5 +4,5 @@ MCData.clear()
 
 MCData.name("zDark")
 
-MCData.loadstring("https://raw.githubusercontent.com/Nerdola12-Dev/zDark/refs/heads/main/zDark_script.py")
 MCData.loadstring("https://raw.githubusercontent.com/Nerdola12-Dev/zDark/refs/heads/main/zDark_hub.py")
+MCData.loadstring("https://raw.githubusercontent.com/Nerdola12-Dev/zDark/refs/heads/main/zDark_script.py")
